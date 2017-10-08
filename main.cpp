d7d7926885707348b6fdb01b278c624d9748225b
Kevin Shaffer <kevinsshaffer@gmail.com>
2017-10-08 16:59:07 -0600

    add files folder with csv files and add readCsvLine method to main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "dog.h"
+
+std::string readCsvLine(std::string);
 
 int main()
 {
+	std::string csvLine = readCsvLine("files\\Dog.csv");
 
+	std::cout << csvLine << std::endl;
+
+	std::cin >> csvLine;
 
 	return 0;
+}
+
+std::string readCsvLine(std::string filePath)
+{
+	std::string line;
+	std::ifstream fs;
+	fs.open(filePath.c_str());
+
+	if (!std::getline(fs, line))
+		throw "Unable to read file.";
+	else
+		return line;
 }
